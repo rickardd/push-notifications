3348cadaa71a2371704185732fd684ee1d97c896
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2022-01-12 19:20:35 +1300

    Improve

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import "./sw.js";
 
-const options = {
+const inputs = {
+  title: document.querySelector('[name="title"]'),
+  body: document.querySelector('[name="body"]'),
+  interval: document.querySelector('[name="interval"]'),
+  start: document.querySelector("button"),
+};
+
+let title = "Default title";
+let interval = 5000;
+let options = {
   body: "Here is a notification body!",
   icon: "duck.png",
   image: "duck.png",
@@ -11,25 +20,26 @@ const options = {
   ],
 };
 
+// Ask user for Push Notification permissions.
 Notification.requestPermission(function (status) {
-  console.log("Notification permission status:", status);
+  console.log("Notification permission status: ---", status);
 });
 
 function displayNotification() {
   if (Notification.permission === "granted") {
     navigator.serviceWorker.getRegistration().then(function (reg) {
       setInterval(() => {
-        reg.showNotification("Survey reminder", options);
-      }, 1000);
+        reg.showNotification(title + Math.random(), options);
+      }, interval);
     });
   }
 }
 
-displayNotification();
-
 function onWebsiteClick(event) {
   self.alert("Navigate to a website");
   console.log("website");
+  self.location.href =
+    "https://www.google.com/search?q=you+clicked+on+the+website+option&rlz=1C5CHFA_enNZ924NZ924&oq=you+clicked+on+the+website+option&aqs=chrome..69i57j69i64.9183j0j15&sourceid=chrome&ie=UTF-8";
 }
 
 function onMyAccountClick(event) {
@@ -61,3 +71,16 @@ self.addEventListener(
   },
   false
 );
+
+function bind() {
+  inputs.start.addEventListener("click", (e) => {
+    e.preventDefault();
+    title = inputs.title.value;
+    options.body = inputs.body.value;
+    interval = parseInt(inputs.interval.value, 10) * 1000; // Converts sec to milliseconds
+    inputs.start.innerText = "WAIT";
+    displayNotification();
+  });
+}
+
+bind();
